9b70f4085dd6df22a0d4c6c1b94907ba7e89d8d5
Yoomin Hwang <yoominh3@gmail.com>
2025-04-21 22:20:14 +0900

    fix: 역량 성장 그래프 누적 count 로 수정 및 DTO에서 totalMilestoneCount 삭제함

--- src/main/java/com/csee/swplus/mileage/milestone/dto/response/MilestoneSemesterTotalPointResponseDto.java
+++ src/main/java/com/csee/swplus/mileage/milestone/dto/response/MilestoneSemesterTotalPointResponseDto.java
@@ -10,5 +10,4 @@ import lombok.NoArgsConstructor;
 public class MilestoneSemesterTotalPointResponseDto {
     private String semester;            // 특정 학기
     private int userMilestoneCount;     // 학생이 해당 학기에 쌓은 마일스톤 역량 개수
-    private int totalMilestoneCount;   // 해당 학기에 쌓을 수 있는 마일스톤 역량 total 개수
 }
