957d7774098cb88c04e4ec52825bbb77d9a77889
Yoomin Hwang <yoominh3@gmail.com>
2025-05-07 01:17:39 +0900

    feat: AwardResponseDto 완성

--- src/main/java/com/csee/swplus/mileage/award/dto/AwardResponseDto.java
+++ src/main/java/com/csee/swplus/mileage/award/dto/AwardResponseDto.java
@@ -3,8 +3,20 @@ package com.csee.swplus.mileage.award.dto;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class AwardResponseDto {
+    private int awardId;
+    private String awardYear;
+    private LocalDate awardDate;
+    private String contestName;
+    private String awardName;
+    private String awardType;
+    private String organization;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
 }
 
